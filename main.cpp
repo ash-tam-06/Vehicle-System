e2b1dc6ebed3628660da016eb91006f991fe6bde
Ash Tamang <asmitatam2006@gmail.com>
2025-05-20 18:42:56 -0700

    setting up vehicle classes

--- main.cpp
+++ main.cpp
@@ -3,13 +3,60 @@
 #include <unordered_map>
 #include <map>
 #include <fstream>
+
 #include "include/vehicle_system.h"
-#include "include/customer_system.h"
+// #include "include/vehicle_system.h"
+// #include "include/customer_system.h"
 
 using namespace std;
 
+enum class VehicleType { Car, Van, Truck };
+
+//base class
+class Vehicle {
+public:
+    int regNumber;
+    bool available;
+
+    Vehicle(int reg): regNumber{reg}, available{true}{}
+    virtual ~Vehicle() = default;
+
+    int getRegNumber() const {
+        return regNumber;
+    }
+    bool isAvailable() const {
+        return available;
+    }
+    void setAvailable(bool status) {
+        available = status;
+    }
+
+    virtual VehicleType getType() const = 0;
+};
+
+// Derived classes
+class Car : public Vehicle {
+public:
+    Car(int reg) : Vehicle(reg) {}
+    VehicleType getType() const override { return VehicleType::Car; }
+};
+
+class Van : public Vehicle {
+public:
+    Van(int reg) : Vehicle(reg) {}
+    VehicleType getType() const override { return VehicleType::Van; }
+};
+
+class Truck : public Vehicle {
+public:
+    Truck(int reg) : Vehicle(reg) {}
+    VehicleType getType() const override { return VehicleType::Truck; }
+};
+
+
 int main(){
     //User interface: gives options for user on what they could do
-    menu();
-    return 0;
+    // menu();
+    // return 0;
+
 }
