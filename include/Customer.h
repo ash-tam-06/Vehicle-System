f67d0a62e4d1f76b3294c8340b5afa24d42b2cd5
Ash Tamang <asmitatam2006@gmail.com>
2025-05-20 19:23:21 -0700

    created customer class

---
+++ include/Customer.h
@@ -0,0 +1,32 @@
+//
+// Created by Asmita Tamang on 5/20/25.
+//
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+#include "Vehicle.h"
+
+using namespace std;
+
+class Customer {
+private:
+    string name;
+    shared_ptr<Vehicle> rentedVehicle;
+
+public:
+    Customer(string name);
+
+    string getName() const;
+
+    void rentVehicle(shared_ptr<Vehicle> vehicle);
+
+    void returnVehicle();
+
+    bool hasVehicle() const;
+
+    shared_ptr<Vehicle> getVehicle() const ;
+};
+
+
+
+#endif //CUSTOMER_H
---
+++ src/Customer.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Asmita Tamang on 5/20/25.
+//
+
+#include <utility>
+
+#include "../include/Customer.h"
+#include <iostream>
+
+using namespace std;
+
+Customer::Customer(string name) : name(move(name)), rentedVehicle(nullptr) {
+    cout << "Customer created" << endl;
+}
+
+string Customer::getName() const { return name; }
+
+void Customer::rentVehicle(shared_ptr<Vehicle> vehicle) {
+    rentedVehicle = vehicle;
+    rentedVehicle->setAvailable(false);
+}
+
+void Customer::returnVehicle() {
+    if (rentedVehicle) {
+        rentedVehicle->setAvailable(true);
+        rentedVehicle = nullptr;
+    }
+}
+
+bool Customer::hasVehicle() const {
+    return rentedVehicle != nullptr;
+}
+
+shared_ptr<Vehicle> Customer::getVehicle() const {
+    return rentedVehicle;
+}
